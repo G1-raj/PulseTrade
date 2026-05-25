b8bb3653c718b36873c631a72791740f0048671a
G1-raj <gullurajput18@gmail.com>
2026-05-25 14:45:09 +0530

    Initial structure

---
+++ frontend/pulsetrade/lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello pulse trade"),
+        ),
+      ),
+    );
+  }
+}
