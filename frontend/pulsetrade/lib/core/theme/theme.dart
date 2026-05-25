60edcb99a64922a367c21e8ced1ce87b508b41d1
G1-raj <gullurajput18@gmail.com>
2026-05-25 14:41:14 +0530

    Centralised the theme components of the app

---
+++ frontend/pulsetrade/lib/core/theme/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/animation.dart';
+
+class AppTheme {
+
+  static const backgroundColor = Color.fromRGBO(9, 18, 29, 1);
+  static const mainButtonColor = Color.fromRGBO(23, 187, 116, 1);
+  static const subButtonColor = Color.fromRGBO(85, 113, 102, 1);
+  static const buttonTextColor = Color.fromRGBO(156, 253, 208, 1);
+  static const textButtonColor = Color.fromRGBO(176, 175, 179, 1);
+
+  static const inputFieldColor = Color.fromRGBO(38, 42, 53, 1);
+  static const inputFieldBorderColor = Color.fromRGBO(58, 66, 76, 1);
+
+  static const greyTextColor = Color.fromRGBO(140, 141, 148, 1);
+
+
+}
